70093853fc533c010e5ccd823509debdf037e36f
Miguel <fagundezdev@gmail.com>
2020-10-27 14:54:20 -0400

    Adding dependencies and initial fragments to navigation

---
+++ app/src/main/java/com/devproject/miguelfagundez/navcompapp/views/ThirdFragment.kt
@@ -0,0 +1,7 @@
+package com.devproject.miguelfagundez.navcompapp.views
+
+import androidx.fragment.app.Fragment
+import com.devproject.miguelfagundez.navcompapp.R
+
+class ThirdFragment : Fragment(R.layout.fragment_third) {
+}
