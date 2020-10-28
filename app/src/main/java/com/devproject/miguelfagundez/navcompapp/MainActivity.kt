58dce46ff13ea77ed9cbbf6ef3a4574733038c3b
Miguel <fagundezdev@gmail.com>
2020-10-28 16:52:10 -0400

    Added a toolbar and setup action bar with navegation

--- app/src/main/java/com/devproject/miguelfagundez/navcompapp/MainActivity.kt
+++ app/src/main/java/com/devproject/miguelfagundez/navcompapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.devproject.miguelfagundez.navcompapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navFragmentContainer = supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
+        navController = navFragmentContainer.findNavController()
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
